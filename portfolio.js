c98f1fdd893b0d22f18fe15b1b547615208c24d2
vanshika501 <151563735+vanshika501@users.noreply.github.com>
2023-11-21 00:30:01 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Front-End-developer.","CPP Programmer.","Python Programmer."],
+    typeSpeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+});
